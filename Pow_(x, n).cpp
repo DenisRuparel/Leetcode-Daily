0574bb987ad165d407370a7e9a81da8a0eda8573
Denis Ruparel <deniskalpeshbhai436@gmail.com>
2024-12-27 08:35:00 +0530

    day-57

---
+++ Pow_(x, n).cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+    public:
+        double myPow(double x, int n) {
+            return pow(x, n);
+
+            if (n == 0) return 1.0; 
+
+            // long long N = n;  
+            // if (N < 0) {
+            //     x = 1 / x;
+            //     N = -N;
+            // }
+
+            // double ans = 1.0;
+            // while (N > 0) {
+            //     if (N % 2 == 1) 
+            //         ans *= x;  
+            //     x *= x;  
+            //     N /= 2;  
+            // }
+            // return ans;
+        }
+    };
