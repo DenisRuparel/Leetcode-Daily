62fd0ce5d0b344fe7f8e354e58e874cfaa901d49
Denis Ruparel <deniskalpeshbhai436@gmail.com>
2025-01-31 17:32:48 +0530

    day-22

---
+++ Add_Strings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        sys.set_int_max_str_digits(10000)
+        int_num1 = int (num1)
+        int_num2 = int (num2)
+        
+        result = int_num1 + int_num2
+        
+        return str(result)
