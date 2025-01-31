a700dfc2c68e7fdfdf3f0390006eda9158a5e5b0
Denis Ruparel <deniskalpeshbhai436@gmail.com>
2025-01-31 17:23:39 +0530

    day-22

---
+++ Multiply_Strings.py
@@ -0,0 +1,8 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        int_num1 = int(num1)
+        int_num2 = int(num2)
+        
+        result = int_num1 * int_num2
+        
+        return str(result)
