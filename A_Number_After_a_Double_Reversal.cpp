2076e06fdacd0a7ed4df54f28037ca1def98e834
Denis Ruparel <deniskalpeshbhai436@gmail.com>
2025-02-02 11:44:49 +0530

    fix A_Number_After_a_Double_Reversal.cpp

--- A_Number_After_a_Double_Reversal.cpp
+++ A_Number_After_a_Double_Reversal.cpp
@@ -4,7 +4,8 @@ using namespace std;
 class Solution {
 public:
     bool isSameAfterReversals(int num) {
-        if (num == 0) return true;
+        if (num == 0)  
+            return true;
         return num % 10 != 0;
     }
 };
