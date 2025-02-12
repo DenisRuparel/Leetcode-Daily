76090e125d75a9508811ba2c120a766d07a2db6d
Denis Ruparel <deniskalpeshbhai436@gmail.com>
2025-02-12 21:29:26 +0530

    day-34

--- Kids_With_the_Greatest_Number_of_Candies.cpp
+++ Kids_With_the_Greatest_Number_of_Candies.cpp
@@ -4,6 +4,7 @@ class Solution{
 public:
     vector<bool> kidsWithCandies(vector<int> &candies, int extraCandies){
         int maxCandy = *max_element(candies.begin(), candies.end());
+    
         vector<bool> ans;
 
         for (int i = 0; i < candies.size(); i++){
