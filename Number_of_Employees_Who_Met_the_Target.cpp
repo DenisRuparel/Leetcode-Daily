53aef93f30cea70f1448addf75965ab6e2a6c397
Denis Ruparel <deniskalpeshbhai436@gmail.com>
2025-02-12 21:35:44 +0530

    day-34

---
+++ Number_of_Employees_Who_Met_the_Target.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int> &hours, int target){
+        int counter = 0;
+        for (int i = 0; i < hours.size(); i++){
+            if (hours[i] >= target)
+                counter++;
+        }
+        return counter;
+    }
+};
