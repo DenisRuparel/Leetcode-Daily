bca6b48dbb422e49c3ac5844594d0c4df465736a
Denis Ruparel <deniskalpeshbhai436@gmail.com>
2025-05-21 15:47:12 +0530

    day-73

---
+++ Single_Element_in_a_Sorted_Array.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int x = 0;
+        for(int i=0; i<nums.size(); i++) 
+            x ^= nums[i];
+        return x;
+    }
+};
