0658e911b3754e7fe36a5100a6ca91d3ac3cc140
Denis Ruparel <deniskalpeshbhai436@gmail.com>
2025-05-22 13:19:49 +0530

    day-74

---
+++ Sort_Colors.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+#include<vector>
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+    }
+};
