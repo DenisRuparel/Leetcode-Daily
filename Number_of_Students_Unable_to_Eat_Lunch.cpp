9a133997adafbfcd5f6a1d3baf8b49c4bedf76dd
DenisRuparel <deniskalpeshbhai436@gmail.com>
2025-05-26 13:44:45 +0530

    day-76

---
+++ Number_of_Students_Unable_to_Eat_Lunch.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        queue<int> studentQueue, sandwichQueue;
+
+        for (int s : students) 
+            studentQueue.push(s);
+        for (int s : sandwiches) 
+            sandwichQueue.push(s);
+
+        int count = 0;
+        while (!studentQueue.empty() && count < studentQueue.size()) {
+            if (studentQueue.front() == sandwichQueue.front()) {
+                studentQueue.pop();
+                sandwichQueue.pop();
+                count = 0; 
+            } else {
+                studentQueue.push(studentQueue.front());
+                studentQueue.pop();
+                count++;
+            }
+        }
+
+        return studentQueue.size();
+    }
+};
